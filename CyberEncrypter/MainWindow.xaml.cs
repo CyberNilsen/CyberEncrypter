985599e0eb23fc841d40922e45508ac17f390a6b
Barnegraver <andreasnilsen07@hotmail.com>
2025-04-02 18:42:56 +0200

    Add project files.

---
+++ CyberEncrypter/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace CyberEncrypter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            SetSizeAndPositionOnPrimaryScreen();
+        }
+        private void SetSizeAndPositionOnPrimaryScreen()
+        {
+     
+            var primaryScreen = System.Windows.SystemParameters.WorkArea;
+            
+            
+            double widthRatio = 0.75;
+            double heightRatio = 0.75;
+            
+            this.Width = primaryScreen.Width * widthRatio;
+            this.Height = primaryScreen.Height * heightRatio;
+            
+            
+            
+            
+            
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
