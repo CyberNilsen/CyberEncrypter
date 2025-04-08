5ce155a0503c6e488e850735bba596d3162946bc
Barnegraver <andreasnilsen07@hotmail.com>
2025-04-08 15:50:07 +0200

    UI ferdig, har ikke lagd UAC for key management og settings dette skal altså endres på.

---
+++ CyberEncrypter/View/Dashboard.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CyberEncrypter;
+
+
+namespace CyberEncrypter.View
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : UserControl
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Encryptfiles_Click(object sender, RoutedEventArgs e)
+        {
+            var MainWindow = (MainWindow)Application.Current.MainWindow;
+            MainWindow.DashboardContent.Content = new Encrypt();
+        }
+
+        private void Decrypt(object sender, RoutedEventArgs e)
+        {
+            var MainWindow = (MainWindow)Application.Current.MainWindow;
+            MainWindow.DashboardContent.Content = new Decrypt();
+        }
+    }
+}
